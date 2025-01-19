40f1768d3b3b8298b54261364624790b5ffe1f13
水野光琉 <mizuno.hikaru.01@gmail.com>
2025-01-19 21:32:57 +0900

    add:assets gen

---
+++ lib/gen/assets.gen.dart
@@ -0,0 +1,292 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+import 'package:flutter/widgets.dart';
+
+class $AssetsAppGen {
+  const $AssetsAppGen();
+
+  /// File path: assets/app/ui5.png
+  AssetGenImage get ui5 => const AssetGenImage('assets/app/ui5.png');
+
+  /// File path: assets/app/ui6.png
+  AssetGenImage get ui6 => const AssetGenImage('assets/app/ui6.png');
+
+  /// File path: assets/app/ui7.png
+  AssetGenImage get ui7 => const AssetGenImage('assets/app/ui7.png');
+
+  /// File path: assets/app/ui8.png
+  AssetGenImage get ui8 => const AssetGenImage('assets/app/ui8.png');
+
+  /// File path: assets/app/ui9.png
+  AssetGenImage get ui9 => const AssetGenImage('assets/app/ui9.png');
+
+  /// List of all assets
+  List<AssetGenImage> get values => [ui5, ui6, ui7, ui8, ui9];
+}
+
+class $AssetsArchitectureGen {
+  const $AssetsArchitectureGen();
+
+  /// File path: assets/architecture/NodeDockerMysqlRestAPI.png
+  AssetGenImage get nodeDockerMysqlRestAPI =>
+      const AssetGenImage('assets/architecture/NodeDockerMysqlRestAPI.png');
+
+  /// File path: assets/architecture/NodeDockerPostgresqlGraphql.png
+  AssetGenImage get nodeDockerPostgresqlGraphql => const AssetGenImage(
+      'assets/architecture/NodeDockerPostgresqlGraphql.png');
+
+  /// File path: assets/architecture/NodeDockerPostgresqlRestAPI.png
+  AssetGenImage get nodeDockerPostgresqlRestAPI => const AssetGenImage(
+      'assets/architecture/NodeDockerPostgresqlRestAPI.png');
+
+  /// File path: assets/architecture/PythonDockerMysqlRestAPI.png
+  AssetGenImage get pythonDockerMysqlRestAPI =>
+      const AssetGenImage('assets/architecture/PythonDockerMysqlRestAPI.png');
+
+  /// File path: assets/architecture/PythonDockerPostgresqlRestAPI.png
+  AssetGenImage get pythonDockerPostgresqlRestAPI => const AssetGenImage(
+      'assets/architecture/PythonDockerPostgresqlRestAPI.png');
+
+  /// File path: assets/architecture/TypeDockerMysqlRestAPI.png
+  AssetGenImage get typeDockerMysqlRestAPI =>
+      const AssetGenImage('assets/architecture/TypeDockerMysqlRestAPI.png');
+
+  /// File path: assets/architecture/TypeDockerPosgresqlRestAPI.png
+  AssetGenImage get typeDockerPosgresqlRestAPI =>
+      const AssetGenImage('assets/architecture/TypeDockerPosgresqlRestAPI.png');
+
+  /// File path: assets/architecture/architecture_1.png
+  AssetGenImage get architecture1 =>
+      const AssetGenImage('assets/architecture/architecture_1.png');
+
+  /// File path: assets/architecture/architecture_3.png
+  AssetGenImage get architecture3 =>
+      const AssetGenImage('assets/architecture/architecture_3.png');
+
+  /// File path: assets/architecture/architecture_9.png
+  AssetGenImage get architecture9 =>
+      const AssetGenImage('assets/architecture/architecture_9.png');
+
+  /// File path: assets/architecture/architecture_node.png
+  AssetGenImage get architectureNode =>
+      const AssetGenImage('assets/architecture/architecture_node.png');
+
+  /// File path: assets/architecture/architecture_unity.png
+  AssetGenImage get architectureUnity =>
+      const AssetGenImage('assets/architecture/architecture_unity.png');
+
+  /// List of all assets
+  List<AssetGenImage> get values => [
+        nodeDockerMysqlRestAPI,
+        nodeDockerPostgresqlGraphql,
+        nodeDockerPostgresqlRestAPI,
+        pythonDockerMysqlRestAPI,
+        pythonDockerPostgresqlRestAPI,
+        typeDockerMysqlRestAPI,
+        typeDockerPosgresqlRestAPI,
+        architecture1,
+        architecture3,
+        architecture9,
+        architectureNode,
+        architectureUnity
+      ];
+}
+
+class $AssetsProjectsGen {
+  const $AssetsProjectsGen();
+
+  /// File path: assets/projects/flutter_food.png
+  AssetGenImage get flutterFood =>
+      const AssetGenImage('assets/projects/flutter_food.png');
+
+  /// File path: assets/projects/flutter_instagram.png
+  AssetGenImage get flutterInstagram =>
+      const AssetGenImage('assets/projects/flutter_instagram.png');
+
+  /// File path: assets/projects/flutter_memo01.png
+  AssetGenImage get flutterMemo01 =>
+      const AssetGenImage('assets/projects/flutter_memo01.png');
+
+  /// File path: assets/projects/flutter_memo02.png
+  AssetGenImage get flutterMemo02 =>
+      const AssetGenImage('assets/projects/flutter_memo02.png');
+
+  /// File path: assets/projects/flutter_tiktok.png
+  AssetGenImage get flutterTiktok =>
+      const AssetGenImage('assets/projects/flutter_tiktok.png');
+
+  /// File path: assets/projects/flutter_translate.png
+  AssetGenImage get flutterTranslate =>
+      const AssetGenImage('assets/projects/flutter_translate.png');
+
+  /// File path: assets/projects/flutter_translate01.png
+  AssetGenImage get flutterTranslate01 =>
+      const AssetGenImage('assets/projects/flutter_translate01.png');
+
+  /// File path: assets/projects/flutter_translate02.png
+  AssetGenImage get flutterTranslate02 =>
+      const AssetGenImage('assets/projects/flutter_translate02.png');
+
+  /// File path: assets/projects/swiftui_airbnb.png
+  AssetGenImage get swiftuiAirbnb =>
+      const AssetGenImage('assets/projects/swiftui_airbnb.png');
+
+  /// File path: assets/projects/swiftui_crypto.png
+  AssetGenImage get swiftuiCrypto =>
+      const AssetGenImage('assets/projects/swiftui_crypto.png');
+
+  /// File path: assets/projects/swiftui_instagram.png
+  AssetGenImage get swiftuiInstagram =>
+      const AssetGenImage('assets/projects/swiftui_instagram.png');
+
+  /// File path: assets/projects/swiftui_messanger.png
+  AssetGenImage get swiftuiMessanger =>
+      const AssetGenImage('assets/projects/swiftui_messanger.png');
+
+  /// File path: assets/projects/swiftui_threads.png
+  AssetGenImage get swiftuiThreads =>
+      const AssetGenImage('assets/projects/swiftui_threads.png');
+
+  /// File path: assets/projects/swiftui_tiktok.png
+  AssetGenImage get swiftuiTiktok =>
+      const AssetGenImage('assets/projects/swiftui_tiktok.png');
+
+  /// List of all assets
+  List<AssetGenImage> get values => [
+        flutterFood,
+        flutterInstagram,
+        flutterMemo01,
+        flutterMemo02,
+        flutterTiktok,
+        flutterTranslate,
+        flutterTranslate01,
+        flutterTranslate02,
+        swiftuiAirbnb,
+        swiftuiCrypto,
+        swiftuiInstagram,
+        swiftuiMessanger,
+        swiftuiThreads,
+        swiftuiTiktok
+      ];
+}
+
+class $AssetsUtilsGen {
+  const $AssetsUtilsGen();
+
+  /// File path: assets/utils/apple_store.png
+  AssetGenImage get appleStore =>
+      const AssetGenImage('assets/utils/apple_store.png');
+
+  /// File path: assets/utils/apple_store_icon.png
+  AssetGenImage get appleStoreIcon =>
+      const AssetGenImage('assets/utils/apple_store_icon.png');
+
+  /// File path: assets/utils/play_store.png
+  AssetGenImage get playStore =>
+      const AssetGenImage('assets/utils/play_store.png');
+
+  /// File path: assets/utils/play_storeicon.png
+  AssetGenImage get playStoreicon =>
+      const AssetGenImage('assets/utils/play_storeicon.png');
+
+  /// List of all assets
+  List<AssetGenImage> get values =>
+      [appleStore, appleStoreIcon, playStore, playStoreicon];
+}
+
+class Assets {
+  Assets._();
+
+  static const $AssetsAppGen app = $AssetsAppGen();
+  static const $AssetsArchitectureGen architecture = $AssetsArchitectureGen();
+  static const $AssetsProjectsGen projects = $AssetsProjectsGen();
+  static const $AssetsUtilsGen utils = $AssetsUtilsGen();
+}
+
+class AssetGenImage {
+  const AssetGenImage(
+    this._assetName, {
+    this.size,
+    this.flavors = const {},
+  });
+
+  final String _assetName;
+
+  final Size? size;
+  final Set<String> flavors;
+
+  Image image({
+    Key? key,
+    AssetBundle? bundle,
+    ImageFrameBuilder? frameBuilder,
+    ImageErrorWidgetBuilder? errorBuilder,
+    String? semanticLabel,
+    bool excludeFromSemantics = false,
+    double? scale,
+    double? width,
+    double? height,
+    Color? color,
+    Animation<double>? opacity,
+    BlendMode? colorBlendMode,
+    BoxFit? fit,
+    AlignmentGeometry alignment = Alignment.center,
+    ImageRepeat repeat = ImageRepeat.noRepeat,
+    Rect? centerSlice,
+    bool matchTextDirection = false,
+    bool gaplessPlayback = true,
+    bool isAntiAlias = false,
+    String? package,
+    FilterQuality filterQuality = FilterQuality.low,
+    int? cacheWidth,
+    int? cacheHeight,
+  }) {
+    return Image.asset(
+      _assetName,
+      key: key,
+      bundle: bundle,
+      frameBuilder: frameBuilder,
+      errorBuilder: errorBuilder,
+      semanticLabel: semanticLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      scale: scale,
+      width: width,
+      height: height,
+      color: color,
+      opacity: opacity,
+      colorBlendMode: colorBlendMode,
+      fit: fit,
+      alignment: alignment,
+      repeat: repeat,
+      centerSlice: centerSlice,
+      matchTextDirection: matchTextDirection,
+      gaplessPlayback: gaplessPlayback,
+      isAntiAlias: isAntiAlias,
+      package: package,
+      filterQuality: filterQuality,
+      cacheWidth: cacheWidth,
+      cacheHeight: cacheHeight,
+    );
+  }
+
+  ImageProvider provider({
+    AssetBundle? bundle,
+    String? package,
+  }) {
+    return AssetImage(
+      _assetName,
+      bundle: bundle,
+      package: package,
+    );
+  }
+
+  String get path => _assetName;
+
+  String get keyName => _assetName;
+}
